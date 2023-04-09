64bb31c63cfd80581e8e5d93e50d190f0b681fba
IgorLunt <tcfqa1989@gmail.com>
2023-04-09 15:02:09 +0300

    add project

---
+++ pages/main_page.py
@@ -0,0 +1,99 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from base.base_class import Base
+from utilities.logger import Logger
+
+
+class Main_page(Base):
+
+
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    #Locators
+
+    select_product_1 = "//button[@id = 'add-to-cart-sauce-labs-backpack']"
+    select_product_2 = "//button[@id = 'add-to-cart-sauce-labs-bike-light']"
+    select_product_3 = "//button[@id = 'add-to-cart-sauce-labs-bolt-t-shirt']"
+    cart = "//div[@id = 'shopping_cart_container']"
+    menu = "//button[@id = 'react-burger-menu-btn']"
+    about_page = "//a[@id = 'about_sidebar_link']"
+
+
+    #Getters
+
+    def get_select_product_1(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_1)))
+
+    def get_select_product_2(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_2)))
+
+    def get_select_product_3(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.select_product_3)))
+
+    def get_cart(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.cart)))
+
+    def get_menu(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.menu)))
+
+    def get_about_page(self):
+        return WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, self.about_page)))
+
+    #Actions
+
+
+    def click_select_product_1(self):
+        self.get_select_product_1().click()
+        print("Click select_product_1")
+
+    def click_select_product_2(self):
+        self.get_select_product_2().click()
+        print("Click select_product_2")
+
+    def click_select_product_3(self):
+        self.get_select_product_3().click()
+        print("Click select_product_3")
+
+    def click_cart(self):
+        self.get_cart().click()
+        print("Click cart")
+
+    def click_menu(self):
+        self.get_menu().click()
+        print("Click menu")
+
+    def click_about_page(self):
+        self.get_about_page().click()
+        print("Click about page")
+
+    # Methods
+    def select_products_1(self):
+        with allure.step("Select products 1"):
+            Logger.add_start_step(method="select_products_1")
+            self.get_current_url()
+            self.click_select_product_1()
+            self.click_cart()
+            Logger.add_end_step(url=self.driver.current_url, method="select_products_1")
+
+    def select_products_2(self):
+        self.get_current_url()
+        self.click_select_product_2()
+        self.click_cart()
+
+    def select_products_3(self):
+        self.get_current_url()
+        self.click_select_product_3()
+        self.click_cart()
+
+    def select_menu_about(self):
+        self.get_current_url()
+        self.click_menu()
+        self.click_about_page()
+        self.assert_url('https://saucelabs.com/')
+
---
+++ tests/test_buy_product.py
@@ -0,0 +1,96 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import allure
+from pages.cart_page import Cart_page
+from pages.client_information_page import Client_information_page
+from pages.finish_button_page import Finish_page
+from pages.login_page import Login_page
+from pages.main_page import Main_page
+from pages.payment_page import Payment_page
+from utilities.logger import Logger
+
+
+#@pytest.mark.run(order=3)
+@allure.description("Test buy product 1")
+def test_buy_product_1(set_up, set_group):
+    options = Options()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(executable_path="C:\\Users\\user\\Desktop\\Selenium_Framework\\chromedriver.exe", chrome_options=options)
+    print("Start test_1")
+
+    login = Login_page(driver)
+    login.authorization()
+
+    mp = Main_page(driver)
+    mp.select_products_1()
+
+    cp = Cart_page(driver)
+    cp.click_checkout_button()
+
+    cip = Client_information_page(driver)
+    cip.input_information()
+
+    p = Payment_page(driver)
+    p.click_finish_button()
+
+    f = Finish_page(driver)
+    f.finish()
+
+    print("Finish Test_1")
+    #time.sleep(10)
+    driver.quit()
+
+
+#@pytest.mark.run(order=2)
+# def test_buy_product_2(set_up, set_group):
+#     options = Options()
+#     options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#     driver = webdriver.Chrome(executable_path="C:\\Users\\user\\Desktop\\Selenium_Framework\\chromedriver.exe", chrome_options=options)
+#     print("Start test_2")
+#
+#     login = Login_page(driver)
+#     login.authorization()
+#
+#     mp = Main_page(driver)
+#     mp.select_products_2()
+#
+#     cp = Cart_page(driver)
+#     cp.click_checkout_button()
+#
+#     print("Finish Test_2")
+#     #time.sleep(10)
+#     driver.quit()
+#
+#
+# #@pytest.mark.run(order=1)
+# def test_buy_product_3(set_up, set_group):
+#     options = Options()
+#     options.add_experimental_option('excludeSwitches', ['enable-logging'])
+#     driver = webdriver.Chrome(executable_path="C:\\Users\\user\\Desktop\\Selenium_Framework\\chromedriver.exe", chrome_options=options)
+#     print("Start test")
+#
+#     login = Login_page(driver)
+#     login.authorization()
+#
+#     mp = Main_page(driver)
+#     mp.select_products_3()
+#
+#     cp = Cart_page(driver)
+#     cp.click_checkout_button()
+#
+#     print("Finish Test_3")
+#     #time.sleep(10)
+#     driver.quit()
+
+
+
+
+
+
+
+
